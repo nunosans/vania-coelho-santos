480916bcbe700fbdf1f946bcc7d8e39578a3220c
Nuno Coelho Santos <nunocs@google.com>
2018-08-12 14:39:11 +0100

    Do redirection to the English page using HTML

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-function comefrom() {
-	$lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-	if (substr($lang, 0, 2) == 'pt') { header("Location: /pt/");}
-	else { header("Location: /en/");}
-	}
-comefrom();
-?>
